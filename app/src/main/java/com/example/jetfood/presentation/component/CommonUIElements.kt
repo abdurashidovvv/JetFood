892ec0f4254f473c1d2eb3d2ffac27e0cf0be077
abdurashidovvv <abdurashidovsaydullo35@gmail.com>
2024-02-17 20:54:47 +0000

    ShowLoading indicator

---
+++ app/src/main/java/com/example/jetfood/presentation/component/CommonUIElements.kt
@@ -0,0 +1,22 @@
+package com.example.jetfood.presentation.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShowLoading() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
